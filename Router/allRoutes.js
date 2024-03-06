c9b5ac703933121841762694b426633243d6f8f3
peakypeakygithub <baboon2303@gmail.com>
2024-03-06 22:50:22 +0100

    #

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -2,7 +2,12 @@ import Route from "./Route.js";
 
 //Définir ici vos routes
 export const allRoutes = [
-    new Route("/", "Accueil", "/pages/home.html"),];
+    new Route("/", "Accueil", "/pages/home.html"),
+    new Route("/signin","Connexion","pages/auth/signin.html"),
+    new Route("/signup","Inscription","pages/auth/signup.html"),
+    new Route("/account","Mon compte","pages/auth/account.html"),
+    new Route("/editPassword","Changement de mot de passe","pages/auth/editPassword.html"),
+];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
 export const websiteName = "Arcadia";
